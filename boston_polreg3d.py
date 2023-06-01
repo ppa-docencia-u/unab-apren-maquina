22b3a003471d32794d6f45f42842a84461b9bdbf
Pablo Poblete Arrué <101666847+ppa-docencia-u@users.noreply.github.com>
2023-06-01 11:50:26 -0400

    Add files via upload

---
+++ boston_polreg3d.py
@@ -0,0 +1,65 @@
+# Polinomial Regression (3D)
+# Cap. 6 pag. 133
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.linear_model import LinearRegression
+
+# Boston Housing Dataset
+# https://www.cs.toronto.edu/~delve/data/boston/bostonDetail.html
+# Polynomial Regression
+#  
+# # Solo para pruebas
+data_url = "http://lib.stat.cmu.edu/datasets/boston"
+raw_df = pd.read_csv(data_url, sep="\s+", skiprows=22, header=None)
+data = np.hstack([raw_df.values[::2, :], raw_df.values[1::2, :2]])
+target = raw_df.values[1::2, 2]
+
+# Boston Housing DF
+df = pd.DataFrame(
+    data, 
+    columns= ['CRIM', 'ZN', 'INDUS', 'CHAS', 'NOX', 'RM','AGE', 
+              'DIS', 'RAD', 'TAX', 'PTRATIO', 'B', 'LSTAT']
+)
+df['MEDV'] = target
+
+#X = pd.DataFrame(np.c_[df['LSTAT'], df['RM']], columns = ['LSTAT','RM'])
+X = df[['LSTAT', 'RM']]
+y = df['MEDV']
+
+# Plotting the 3D Hyperplane
+# Cap. 6 pag. 133
+fig = plt.figure(figsize=(18,15))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(X['LSTAT'],
+           X['RM'],
+           y, 
+           c='b'
+    )
+ax.set_xlabel("LSTAT")
+ax.set_ylabel("RM")
+ax.set_zlabel("MEDV")
+
+#---create a meshgrid of all the values for LSTAT and RM---
+x_surf = np.arange(0, 40, 1)   #---for LSTAT---
+y_surf = np.arange(0, 10, 1)   #---for RM---
+x_surf, y_surf = np.meshgrid(x_surf, y_surf)
+
+# Aplica Regresión Lineal
+model = LinearRegression()
+model.fit(X, y)
+
+#---calculate z(MEDC) based on the model---
+z = lambda x,y: (model.intercept_ + 
+                 model.coef_[0] * x + 
+                 model.coef_[1] * y
+                )
+ax.plot_surface(x_surf, y_surf, z(x_surf,y_surf),
+                rstride=1,
+                cstride=1,
+                color='None',
+                alpha = 0.4
+                )
+plt.show()
